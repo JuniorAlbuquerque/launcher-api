c10f45afe32d4dd54d57f7d52678903d383a9180
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2022-03-15 19:30:57 -0300

    fix: return ping result

--- src/services/ping/index.ts
+++ src/services/ping/index.ts
@@ -4,7 +4,11 @@ import { socketEmit } from '../socket'
 const getPingStatus = (addr: string, delay = 1500) => {
   const calcPing = () =>
     ping.promise.probe(addr).then(function (res) {
+      if (!res.max || !res.min) return
+
       const pingResult = Math.round((Number(res.max) + Number(res.min)) / 2)
+      if (isNaN(pingResult)) return
+
       socketEmit({
         topic: 'PING',
         message: pingResult
